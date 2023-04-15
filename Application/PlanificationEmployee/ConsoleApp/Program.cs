1b0eeb3d7dcbb0193418dbe72edea01825752505
zakariabellil <zakaria.bellil@polymtl.ca>
2023-04-15 13:31:23 -0400

    ajouter le mappeur + la console

---
+++ Application/PlanificationEmployee/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using PlanificationEmployee;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mappeurJson mappeur = new mappeurJson();
+            JObject jsonObjet = mappeur.getDataFromJsonFile("C:\\Users\\15142\\Downloads\\json_1\\FeuilleTempsExemple.json");
+            feuilleDeTemps feuilleDeTemps = new feuilleDeTemps(jsonObjet);
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ Application/PlanificationEmployee/PlanificationEmployee/feuilleDeTemps.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanificationEmployee {
+    public class feuilleDeTemps {
+        public int numeroEmploye;
+        public List<Tuple<int, int>> jour1 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> jour2 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> jour3 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> jour4 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> jour5 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> weekend1 = new List<Tuple<int, int>>();
+        public List<Tuple<int, int>> weekend2 = new List<Tuple<int, int>>();
+        public feuilleDeTemps(JObject jsonObjet) {
+            numeroEmploye = (int)jsonObjet["numeroEmploye"];
+      
+            foreach (JToken token in jsonObjet["jour1"])               
+                jour1.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));                
+       
+            foreach (JToken token in jsonObjet["jour2"])               
+                jour2.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));               
+
+            foreach (JToken token in jsonObjet["jour3"])
+                jour3.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));
+
+            foreach (JToken token in jsonObjet["jour4"])
+                jour4.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));
+
+            foreach (JToken token in jsonObjet["jour5"])
+                jour5.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));
+
+            foreach (JToken token in jsonObjet["weekend1"])
+                weekend1.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));
+         
+            foreach (JToken token in jsonObjet["weekend2"])
+                weekend2.Add(new Tuple<int, int>((int)token["projet"], (int)token["minutes"]));
+
+
+        }
+    }
+}
