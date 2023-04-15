6c6d530c3a5c21dfabf6caa0a7d472de632b0eda
Rima Chabane <chabanerima@gmail.com>
2023-04-15 12:17:16 -0400

    Ajout des classes

---
+++ Application/PlanificationEmployee/PlanificationEmployee/JourDeTravail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PlanificationEmployee
+{
+    public class JourDeTravail
+    {
+        public int NumeroJour { get; set; }
+        public List<int> Projets { get; set; }
+        public int TotalMinutes { get; set; }
+
+        public JourDeTravail(int numeroJour)
+        {
+            NumeroJour = numeroJour;
+            Projets = new List<int>();
+        }
+    }
+
+}
---
+++ Application/PlanificationEmployee/PlanificationEmployee/SemaineDeTravail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanificationEmployee
+{
+    class SemaineDeTravail
+    {
+        public int NumeroEmploye { get; set; }
+        public Dictionary<string, JourDeTravail> Jours { get; set; }
+
+        public SemaineDeTravail()
+        {
+            Jours = new Dictionary<string, JourDeTravail>();
+        }
+    }
+}
