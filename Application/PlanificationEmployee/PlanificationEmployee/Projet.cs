e6f4887b82c309b993492981c81679c5caaea743
zakariabellil <zakaria.bellil@polymtl.ca>
2023-04-17 18:43:21 -0400

    ajout

---
+++ Application/PlanificationEmployee/PlanificationEmployee/Projet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanificationEmployee
+{
+    class Projet
+    {
+        public int Code { get; set; } // le code du projet
+      
+        public int MinutesAllouees { get; set; } // le nombre d'heures allouées au projet
+    }
+}
---
+++ Application/PlanificationEmployee/PlanificationEmployee/mappeurJson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PlanificationEmployee
+{
+public class mappeurJson
+    {
+    public JObject getDataFromJsonFile(string inputFilePath) {
+        string jsonString = File.ReadAllText(inputFilePath);
+        JObject jsonObjet = JsonConvert.DeserializeObject<JObject>(jsonString);
+        return jsonObjet;
+        
+        }
+  public JObject getOutPutJson(Dictionary<string, string> data) {        
+            return JObject.FromObject(data);
+        }
+    }
+}
