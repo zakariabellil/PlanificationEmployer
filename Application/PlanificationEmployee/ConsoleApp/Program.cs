3c609ee46f34c03cb4d23fe6453952bfe1599d4b
zakariabellil <zakaria.bellil@polymtl.ca>
2023-04-18 10:24:44 -0400

    ajouter le outputTxt2

--- Application/PlanificationEmployee/ConsoleApp/Program.cs
+++ Application/PlanificationEmployee/ConsoleApp/Program.cs
@@ -10,14 +10,18 @@ namespace ConsoleApp
         {
             mappeurJson mappeur = new mappeurJson();
             Console.WriteLine("Entrez le chemin complet du fichier JSON :");
-            string cheminOriginFichier = Console.ReadLine();
-            string cheminModifieFichier = cheminOriginFichier.Replace("\\", "\\\\");
-            string cheminModifieFichier2 = cheminModifieFichier.Replace("\"", "");
-            JObject jsonObjet = mappeur.getDataFromJsonFile(cheminModifieFichier2);
+            string cheminOriginFichier = Console.ReadLine().Replace("\"", "\\");
+            //string cheminModifieFichier = cheminOriginFichier.Replace("\\", "\\\\");
+            //string cheminModifieFichier2 = cheminModifieFichier.Replace("\"", "");
+            JObject jsonObjet = mappeur.getDataFromJsonFile(cheminOriginFichier);
             feuilleDeTemps feuilleDeTemps = new feuilleDeTemps(jsonObjet);
             validateurFeuilleDeTemps validateurFeuilleDeTemps = new validateurFeuilleDeTemps();
             JObject outPutJsonObjet = validateurFeuilleDeTemps.validerFeuilleDeTemps(feuilleDeTemps);
-            mappeur.getTxtFromJson("C:\\Users\\15142\\Desktop\\test\\PlanificationEmployer\\Application\\PlanificationEmployee\\ConsoleApp", "test.json", outPutJsonObjet);
+            Console.WriteLine("Entrez le chemin complet du fichier sortie JSON (sans backslash à la fin):");
+            string cheminOriginSortieFichier = Console.ReadLine().Replace("\"", "\\");
+            Console.WriteLine("Entrez le nom du fichier sortie JSON (avec .json à la fin) : ");
+            string nom = Console.ReadLine();
+            mappeur.getTxtFromJson(cheminOriginSortieFichier, nom, outPutJsonObjet);
             Console.WriteLine(feuilleDeTemps.ToString());
         }
     }
