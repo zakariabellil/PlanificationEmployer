1dfa9a9413c8d14c28c0de0e3c44c33e9f820359
zakariabellil <zakaria.bellil@polymtl.ca>
2023-04-30 09:26:44 -0400

    interface employer

---
+++ Application/PlanificationEmployee/PlanificationEmployee/feuilleDeTempsEmployer.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlanificationEmployee
+{
+    /// <summary>
+    /// Logique d'interaction pour feuilleDeTempsEmployer.xaml
+    /// </summary>
+    public partial class feuilleDeTempsEmployer : Window
+    {
+        List<rangeFeuilleDeTempsUI> rangeFeuilleDeTempsUIS = new List<rangeFeuilleDeTempsUI>();
+        public feuilleDeTempsEmployer()
+        {
+            InitializeComponent();
+            rangeFeuilleDeTempsUIS.Add(new rangeFeuilleDeTempsUI { CodeProjet = "", Lundi = "", Mardi = "" });
+            dataGrid.ItemsSource = rangeFeuilleDeTempsUIS;
+        }
+
+
+
+        private void Sauvgarder_Click(object sender, RoutedEventArgs e)
+        {
+            List<rangeFeuilleDeTempsUI> test = dataGrid.ItemsSource as List<rangeFeuilleDeTempsUI>;
+        }
+    }
+}
